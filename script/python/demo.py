7ae411bf09e91a1849720e13d593e4cdc5cfe9e6
yanqiang.wyq <yanqiang.wyq@alibaba-inc.com>
2017-02-04 17:01:02 +0800

    add ling regression

---
+++ script/python/demo.py
@@ -0,0 +1,18 @@
+from datetime import datetime, date, time
+
+print 'hello python'
+print 'hello world'
+a = [1,2,3]
+if a:
+    print "something"
+else:
+    print "nothing"
+
+b = []
+if b:
+    print "something"
+else:
+    print "nothing"
+
+dt = datetime(2017, 02, 04, 16, 35, 00)
+print dt.date()
